66e14b51f596ee703cbc87f347b754eb7fc99dd4
JCPrime01 <vanzokiller@gmail.com>
2026-06-17 11:14:11 -0300

    chore: shareCode CR7YGMC4510 -> JHBOR3TAJ4S (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=CR7YGMC4510&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=JHBOR3TAJ4S&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
