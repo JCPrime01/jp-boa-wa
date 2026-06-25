a225ead79a068922566a1c3d83580c8142a7d1dc
JCPrime01 <vanzokiller@gmail.com>
2026-06-25 09:52:57 -0300

    chore: shareCode FKPT43ZGHFL -> NLTMMJZ98BG (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=FKPT43ZGHFL&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=NLTMMJZ98BG&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
