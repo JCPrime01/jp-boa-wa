4096702a922647043fd802aee01b3280bc10e6d1
JCPrime01 <vanzokiller@gmail.com>
2026-06-26 09:29:41 -0300

    chore: shareCode NLTMMJZ98BG -> 67SCFIRLYJY (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=NLTMMJZ98BG&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=67SCFIRLYJY&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
