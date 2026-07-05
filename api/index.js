66c76729601ed4ce584807c0328e997bc7a11459
JCPrime01 <vanzokiller@gmail.com>
2026-07-05 10:48:56 -0300

    chore: shareCode C4JT90WE7OI -> 1I6SQMW6801 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=C4JT90WE7OI&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=1I6SQMW6801&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
