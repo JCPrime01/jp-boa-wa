0db269f035b86532226b2ccb58ca1dd408d2354f
JCPrime01 <vanzokiller@gmail.com>
2026-07-09 08:50:36 -0300

    chore: shareCode XRDPDISNEO5 -> UF9ZLEJ8SSE (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=XRDPDISNEO5&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=UF9ZLEJ8SSE&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
