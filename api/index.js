7e1359fae47ccba2c3e590e8c43c485808819be5
JCPrime01 <vanzokiller@gmail.com>
2026-07-15 11:29:00 -0300

    chore: shareCode 24CF6NBT74A -> ARYUI6B7I83 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=24CF6NBT74A&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=ARYUI6B7I83&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
